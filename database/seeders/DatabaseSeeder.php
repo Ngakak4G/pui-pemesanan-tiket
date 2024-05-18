9bb989fed7ed7ee438b6d525be77880368657123
Feri Setyawan <ferisetyawan1502@gmail.com>
2024-05-18 23:52:42 +0700

    first commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\Category;
+use App\Models\Product;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +16,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+
+        //Untuk Run data Dummy sebanyak 10
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'feri',
+            'email' => 'feri@gmail.com',
+            'password' => Hash::make('12345678'),
         ]);
+
+        //data dummy untuk category
+        Category::factory(10)->create();
+
+        //data dummy Product
+        Product::factory(10)->create();
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
+#Route Login
 Route::get('/', function () {
-    return view('pages.dashboard-general-dashboard');
+    return view('pages.auth.login');
 });
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/home', function () {
+        return view('pages.dashboard-general-dashboard');
+    })->name('home');
+   Route::resource('users', UserController::class);
+    Route::resource('categories', CategoryController::class);
+    Route::resource('products', ProductController::class);
+});
+
